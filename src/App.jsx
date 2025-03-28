7073379ab1428b30bdcbe0c45e40306572239fe8
Ramil <zarram89@gmail.com>
2025-03-28 13:44:35 +0500

    Убирает лишние компоненты

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
 import LoginForm from "./LoginForm";
 
+function getImageUrl(person, size = 's') {
+  return (
+    'https://i.imgur.com/' +
+    person.imageId +
+    size +
+    '.jpg'
+  );
+}
+
+function Avatar({person, size}) {
+  return(
+    <img
+      className="avatar" 
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
+
 export default function App() {
-  return <LoginForm />;
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
 }
