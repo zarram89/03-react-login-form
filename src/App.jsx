67dbb89942141efda259e5993dc118d762033a33
Ramil <zarram89@gmail.com>
2025-03-28 13:54:34 +0500

    Добавляет компонент формы и кнопки формы и удаляет лишний код

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,5 @@
 import LoginForm from "./LoginForm";
 
-function getImageUrl(person, size = 's') {
-  return (
-    'https://i.imgur.com/' +
-    person.imageId +
-    size +
-    '.jpg'
-  );
-}
-
-function Avatar({person, size}) {
-  return(
-    <img
-      className="avatar" 
-      src={getImageUrl(person)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
-  );
-}
-
 export default function App() {
   return (
     <>
