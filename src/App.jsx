c6796322d975882042450de5df579add4f78dbcc
Ramil <zarram89@gmail.com>
2025-04-01 09:25:12 +0500

    Добавляет хуки и контролируемые инпуты

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import LoginForm from "./LoginForm";
 import RegistarationForm from "./RegistrationForm"
 
 export default function App() {
-  const isVisible = false;
+  const isVisible = true;
 
-  const handleSubmit = (e) => {
-	e.preventDefault();
-	const username = e.target.username.value;
-	const password = e.target.password.value;
-	console.log(`Пользователь успешно авторизован:\nЛогин: ${username}\nПароль: ${password}`);
+  const handleSubmit = (e, username, password) => {
+    e.preventDefault();
+    console.log(`Пользователь успешно авторизован:\nЛогин: ${username}\nПароль: ${password}`);
 	};
 
   return isVisible ? (
--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,21 +1,33 @@
+import { useState } from 'react'
 import Button from './Button'
 import './LoginForm.css'
 
 export default function LoginForm({onSubmit}) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleUsernameChange(e) {
+    setUsername(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
   return (
     <div className="auth">
-      <form className="auth__form" action="#" method="post" autoComplete="off" onSubmit={onSubmit}>
+      <form className="auth__form" action="#" method="post" autoComplete="off" onSubmit={(e) => onSubmit(e, username, password)}>
         <h1 className="auth__title">Login Form</h1>
         <div>
           <label>
             <span className="visually-hidden">Enter your name</span>
-            <input type="text" name="username" placeholder="Username" required/>
+            <input type="text" name="username" placeholder="Username" value={username} onChange={handleUsernameChange} required/>
           </label>
         </div>
         <div>
           <label>
             <span className="visually-hidden">Enter your password</span>
-            <input type="password" name="password" placeholder="Password" required/>
+            <input type="password" name="password" placeholder="Password" value={password} onChange={handlePasswordChange} required/>
           </label>
         </div>
         <div>
